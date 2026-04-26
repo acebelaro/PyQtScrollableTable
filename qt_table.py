60caafb8470f34641c88284d09af8ff1fa2e988f
Ace <ac.belaro@gmail.com>
2026-04-26 15:14:05 +0800

    Fixed stylings

--- qt_table.py
+++ qt_table.py
@@ -1,9 +1,12 @@
-from abc import ABC, ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Any, List, NamedTuple, Optional
 from PyQt6 import QtCore
 from PyQt6.QtWidgets import QWidget, QGroupBox, QScrollArea, QLabel, QVBoxLayout
 import uuid
+
+_TABLE_HEADER_CELL_GROUPBOX_NAME = "QtTableHeaderCellGroupBox"
+_TABLE_ROW_GROUPBOX_NAME = "QtTableRowGroupBox"
 
 
 class TableCellUiType(Enum):
@@ -17,7 +20,6 @@ class TableColumnConfig(NamedTuple):
     ui_type: TableCellUiType
     text: str
     width: int
-    object_name: str
 
 
 class TableConfig(NamedTuple):
@@ -26,28 +28,36 @@ class TableConfig(NamedTuple):
     header_cell_css_styles: List[str]
 
 
-# Custom metaclass to resolve conflict between QWidget's metaclass and ABCMeta
-class _TableRowMeta(type(QWidget), ABCMeta):
-    pass
-
-
 class TableRow(QWidget):
 
     def __init__(
         self,
+        width: int,
+        height: int,
         id: str = "",
         data: Optional[Any] = None,
     ):
         super().__init__()
 
+        self._object_name = "TableRow"
         self._id = id
         self._data = data
         if self._id == "":
             self._id = str(uuid.uuid4())
-            # print(f"ID generated: {self._id}")
 
-        self._create_ui()
+        row_group_box = QGroupBox(parent=self)
+        row_group_box.setGeometry(QtCore.QRect(0, 0, width, height))
+        row_group_box.setTitle("")
+        row_group_box.setObjectName(_TABLE_ROW_GROUPBOX_NAME)
+        css_style_text = (
+            f"QGroupBox#{_TABLE_ROW_GROUPBOX_NAME}{{ border: 1px solid black; }}"
+        )
+        row_group_box.setStyleSheet(css_style_text)
 
+        self._create_ui(row_group_box=row_group_box)
+        self.setFixedHeight(height)
+
+    @staticmethod
     @property
     def id(self) -> str:
         return self._id
@@ -57,7 +67,7 @@ class TableRow(QWidget):
         return self._data
 
     @abstractmethod
-    def _create_ui(self) -> None:
+    def _create_ui(self, row_group_box: QGroupBox) -> None:
         raise NotImplementedError()
 
 
@@ -98,7 +108,6 @@ class Table(ABC):
 
         self._scroll_area_content = QWidget()
         self._scroll_area_content.setGeometry(self._scroll_area.rect())
-        self._scroll_area_content.setObjectName("scrollAreaWidgetContents")
         self._scroll_area.setWidget(self._scroll_area_content)
 
         self._table_layout = QVBoxLayout()
@@ -132,51 +141,64 @@ class Table(ABC):
 
     def _create_header_row(self) -> List[QGroupBox]:
         columng_group_boxes: List[QGroupBox] = []
-        group_box_y_pos = 0
+        group_box_x_pos = 0
         table_column_config_count = len(self._table_config.column_configs)
         for index in range(table_column_config_count):
             table_column_config = self._table_config.column_configs[index]
-            columng_group_box = QGroupBox(parent=self._groupbox_container)
-            columng_group_box.setGeometry(
-                QtCore.QRect(
-                    group_box_y_pos,
-                    0,
-                    table_column_config.width,
-                    self._table_config.header_row_height,
-                )
-            )
-            css_style_text = "; ".join(self._table_config.header_cell_css_styles)
-            css_style_text = f"{css_style_text}; border: 1px solid black;"
+            is_last_column = False
             if index != (table_column_config_count - 1):
-                css_style_text = f"{css_style_text} border-right: none;"
-
-            columng_group_box.setStyleSheet(css_style_text)
-            columng_group_box.setTitle("")
-            columng_group_box.setObjectName(table_column_config.object_name)
-
-            column_text_label = QLabel(parent=columng_group_box)
-            column_text_label.setGeometry(
-                QtCore.QRect(
-                    0,
-                    0,
-                    table_column_config.width,
-                    self._table_config.header_row_height,
-                )
+                is_last_column = True
+            columng_group_box = self._create_header_cell_group_box(
+                table_column_config=table_column_config,
+                x_pos=group_box_x_pos,
+                is_last_column=is_last_column,
             )
-            column_text_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-            column_text_label.setObjectName(f"label_{table_column_config.object_name}")
-            column_text_label.setText(table_column_config.text)
-
-            group_box_y_pos = group_box_y_pos + table_column_config.width
+            group_box_x_pos = group_box_x_pos + table_column_config.width
             columng_group_boxes.append(columng_group_box)
-        if group_box_y_pos > self._groupbox_container.width():
+        if group_box_x_pos > self._groupbox_container.width():
             raise ValueError(
                 f"Table '{self._name}' column header total width exceeds given group box container width!"
             )
         return columng_group_boxes
 
-    # @staticmethod
-    # def
+    def _create_header_cell_group_box(
+        self,
+        table_column_config: TableColumnConfig,
+        x_pos: int,
+        is_last_column: bool,
+    ) -> QGroupBox:
+
+        columng_group_box = QGroupBox(parent=self._groupbox_container)
+        columng_group_box.setGeometry(
+            QtCore.QRect(
+                x_pos,
+                0,
+                table_column_config.width,
+                self._table_config.header_row_height,
+            )
+        )
+        css_style_text = "; ".join(self._table_config.header_cell_css_styles)
+        css_style_text = f"{css_style_text}; border: 1px solid black;"
+        if is_last_column:
+            css_style_text = f"{css_style_text} border-right: none;"
+        css_style_text = (
+            f"QGroupBox#{_TABLE_HEADER_CELL_GROUPBOX_NAME}{{ {css_style_text} }}"
+        )
+        columng_group_box.setObjectName(_TABLE_HEADER_CELL_GROUPBOX_NAME)
+        columng_group_box.setStyleSheet(css_style_text)
+        columng_group_box.setTitle("")
+
+        column_text_label = QLabel(parent=columng_group_box)
+        column_text_label.setGeometry(
+            QtCore.QRect(
+                0,
+                0,
+                table_column_config.width,
+                self._table_config.header_row_height,
+            )
+        )
+        column_text_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        column_text_label.setText(table_column_config.text)
 
     @abstractmethod
     def _create_row(self, data: Any) -> TableRow:
--- sample/sample_table.py
+++ sample/sample_table.py
@@ -11,68 +11,15 @@ class SampleTableRow(TableRow):
         data: int,
     ):
         super().__init__(
+            width=730,
+            height=40,
             id="",
             data=data,
         )
 
-        self.setFixedHeight(40)
-        self.setContentsMargins(0, 0, 0, 0)
-
-    def _create_ui(self):
-        # # raise NotImplementedError
-        # self.setGeometry(QtCore.QRect(0, 0, 731, 41))
-        # # self.setTitle("")
-        # self.setObjectName("grpActionRow")
-
-        # self.labelDescription = QtWidgets.QLabel(parent=self)
-        # self.labelDescription.setGeometry(QtCore.QRect(10, 10, 361, 16))
-        # self.labelDescription.setObjectName("labelDescription")
-        # self.labelDescription.setText(f"{self.data}")
-        print("...")
-        ActionRow = self
-        self.grpActionRow = QtWidgets.QGroupBox(parent=ActionRow)
-        self.grpActionRow.setGeometry(QtCore.QRect(0, 0, 731, 41))
-        self.grpActionRow.setTitle("")
-        self.grpActionRow.setObjectName("grpActionRow")
-        self.grpColEnable = QtWidgets.QGroupBox(parent=self.grpActionRow)
-        self.grpColEnable.setGeometry(QtCore.QRect(0, 0, 71, 41))
-        self.grpColEnable.setTitle("")
-        self.grpColEnable.setObjectName("grpColEnable")
-        self.checkEnable = QtWidgets.QCheckBox(parent=self.grpColEnable)
-        self.checkEnable.setGeometry(QtCore.QRect(30, 10, 30, 20))
-        self.checkEnable.setText("")
-        self.checkEnable.setObjectName("checkEnable")
-        self.grpColDescription = QtWidgets.QGroupBox(parent=self.grpActionRow)
-        self.grpColDescription.setGeometry(QtCore.QRect(70, 0, 381, 41))
-        self.grpColDescription.setTitle("")
-        self.grpColDescription.setObjectName("grpColDescription")
-        self.labelDescription = QtWidgets.QLabel(parent=self.grpColDescription)
-        self.labelDescription.setGeometry(QtCore.QRect(10, 10, 361, 16))
-        self.labelDescription.setObjectName("labelDescription")
-        self.grpColButtons = QtWidgets.QGroupBox(parent=self.grpActionRow)
-        self.grpColButtons.setGeometry(QtCore.QRect(560, 0, 171, 41))
-        self.grpColButtons.setTitle("")
-        self.grpColButtons.setObjectName("grpColButtons")
-        self.btnDelete = QtWidgets.QPushButton(parent=self.grpColButtons)
-        self.btnDelete.setGeometry(QtCore.QRect(100, 10, 61, 24))
-        self.btnDelete.setObjectName("btnDelete")
-        self.btnEdit = QtWidgets.QPushButton(parent=self.grpColButtons)
-        self.btnEdit.setGeometry(QtCore.QRect(70, 10, 31, 24))
-        self.btnEdit.setObjectName("btnEdit")
-        self.btnMoveUp = QtWidgets.QPushButton(parent=self.grpColButtons)
-        self.btnMoveUp.setGeometry(QtCore.QRect(40, 10, 31, 24))
-        self.btnMoveUp.setObjectName("btnMoveUp")
-        self.btnMoveDown = QtWidgets.QPushButton(parent=self.grpColButtons)
-        self.btnMoveDown.setGeometry(QtCore.QRect(10, 10, 31, 24))
-        self.btnMoveDown.setObjectName("btnMoveDown")
-        self.grpColDelay = QtWidgets.QGroupBox(parent=self.grpActionRow)
-        self.grpColDelay.setGeometry(QtCore.QRect(450, 0, 111, 41))
-        self.grpColDelay.setTitle("")
-        self.grpColDelay.setObjectName("grpColDelay")
-        self.labelDelay = QtWidgets.QLabel(parent=self.grpColDelay)
-        self.labelDelay.setGeometry(QtCore.QRect(10, 10, 71, 20))
-        self.labelDelay.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.labelDelay.setObjectName("labelDelay")
+    def _create_ui(self, row_group_box: QGroupBox):
+        # TODO: create UI here
+        pass
 
 
 class SampleTable(Table):
@@ -87,25 +34,21 @@ class SampleTable(Table):
                         ui_type=TableCellUiType.CHECKBOX,
                         text="Check",
                         width=70,
-                        object_name="grpCheckCol",
                     ),
                     TableColumnConfig(
                         ui_type=TableCellUiType.READONLY_TEXT,
                         text="Name",
                         width=380,
-                        object_name="grpNameCol",
                     ),
                     TableColumnConfig(
                         ui_type=TableCellUiType.READONLY_TEXT,
                         text="Value",
                         width=110,
-                        object_name="grpValueCol",
                     ),
                     TableColumnConfig(
                         ui_type=TableCellUiType.CUSTOM,
                         text="",
                         width=190,
-                        object_name="grpControlsCol",
                     ),
                 ],
                 header_row_height=40,
